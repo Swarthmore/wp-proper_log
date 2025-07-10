87e6f1808037ef984905fbcbf143ba5c7ab8931d
Les Leach <lleach1@swarthmore.edu>
2025-07-10 17:56:36 +0000

    Format the monolog format

--- proper_log.php
+++ proper_log.php
@@ -10,8 +10,11 @@ require plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\RotatingFileHandler;
+use Monolog\Formatter\LineFormatter;
 
 add_action("aal_insert_log", "proper_log", 5, 1);
+
+define("WP_LOG_PREFIX", "WordPress");
 
 function proper_log($args){
 	$site_name = preg_replace('/^https?:\/\/(.+)$/', "$1", get_site_url());
@@ -29,7 +32,7 @@ function proper_log($args){
 }
 
 function system_log($message) {
-	@openlog("WordPress", LOG_NDELAY, LOG_LOCAL0); 
+	@openlog(WP_LOG_PREFIX, LOG_NDELAY, LOG_LOCAL0); 
 	@syslog(LOG_INFO, $message . "\n");
 	@closelog();
 }
@@ -39,13 +42,26 @@ function monolog_local($message) {
 	if ( ! file_exists( $target_dir ) ) {
     wp_mkdir_p( $target_dir );
 	}
+	// the default date format is "Y-m-d\TH:i:sP"
+	$dateFormat = "M n H:i:s";
+
+	// get the hostname
+	$hostname = preg_replace("/^([^\.]+).*$/", "$1", php_uname("n"));
+
+	// the default output format is "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n"
+	// we now change the default output format according to our needs.
+	$output = "%datetime% $hostname " . WP_LOG_PREFIX . ": %message%\n";
+
+	// finally, create a formatter
+	$formatter = new LineFormatter($output, $dateFormat);	
 
 	// Create a logger instance
 	$log = new Logger('wp_event_log');
 
 	// Create a rotating file handler (daily rotation, keep 3 files)
   $handler = new RotatingFileHandler($target_dir . '/wp_event.log', 3, Logger::DEBUG);
-
+	$handler->setFormatter($formatter);
+	
 	// Attach the handler
 	$log->pushHandler($handler);
 
